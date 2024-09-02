f76adb8d26d96b92e5dd26d957b485331797c9c2
nikmosi <i@nikmosi.ru>
2024-09-02 23:09:16 +0700

    test(tokinizer): test with two names

--- tests/test_tokinizer.py
+++ tests/test_tokinizer.py
@@ -1,8 +1,14 @@
 from hello_bot.tokinizer import Token
 
 
-def test_token():
+def test_nikname_token():
     token = Token.nikname
     catched = token.catch("@nikmosi")
+
+    assert catched == "nikmosi"
+
+def test_nikname_token_with_two_nikname():
+    token = Token.nikname
+    catched = token.catch("@nikmosi @jeezer")
 
     assert catched == "nikmosi"
